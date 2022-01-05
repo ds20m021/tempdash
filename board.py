9581974de94c377ab54f2e0696e0d6ee43f4446e
Olaf <ds20m021@technikum-wien.at>
2022-01-05 21:23:58 +0100

    adds slider

--- board.py
+++ board.py
@@ -15,6 +15,9 @@ def load_data():
     data = data.dropna(axis=0)
 
     data = data.groupby(['year', 'Country']).agg({'AverageTemperature': 'mean'}).reset_index()
+    worldData = data.groupby(['year']).agg({'AverageTemperature': 'mean'}).reset_index()
+    worldData['Country'] = "world"
+    data = pd.concat([data, worldData])
     #data = data.rename(columns={'year': 'index'}).set_index('index')
     return data
 
@@ -24,20 +27,24 @@ data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text('')
 
-dsp = data[(data["Country"] == "Austria") | (data["Country"] == "Norway")]
+year_to_filter = st.slider('year', min_value=1743, max_value=2013, step=1)
+
+dsp = data[data["Country"] == "world"]
+filtered = dsp[dsp["year"] > year_to_filter]
 import matplotlib
 import seaborn as sns
 #matplotlib.rcParams['axes.grid'] = True
 #matplotlib.rcParams['savefig.transparent'] = True
-dsp = data[(data["Country"] == "Austria") | (data["Country"] == "Norway")]
+#dsp = data[(data["Country"] == "Austria") | (data["Country"] == "Norway")]
 fig = plt.figure()
-sns.lineplot(data=dsp, x="year", y="AverageTemperature", hue="Country")
+sns.lineplot(data=filtered, x="year", y="AverageTemperature", alpha=1, palette=['blue'])
+sns.lineplot(data=dsp, x="year", y="AverageTemperature", alpha=0.3, palette=['blue'])
 
 st.pyplot(fig)
 
 
 countries = pd.read_csv('countries.txt')
-mapdata = data[data["year"] == 2010][["Country", "AverageTemperature"]]
+mapdata = data[(data["year"] == year_to_filter) & (data["Country"] != "world")][["Country", "AverageTemperature"]]
 mapdata = countries.merge(mapdata, left_on='Name', right_on='Country', how='left')
 #st.line_chart(dsp['AverageTemperature'])
 
