87c5e25166fa4d61fd75daa8c888421a8aeac985
Olaf <ds20m021@technikum-wien.at>
2022-01-17 21:19:59 +0100

    Uses Azure prediction url

--- board.py
+++ board.py
@@ -19,7 +19,7 @@ def load_data():
 
     data = data.groupby(['year', 'Country']).agg({'AverageTemperature': 'mean'}).reset_index()
     worldData = data.groupby(['year']).agg({'AverageTemperature': 'mean'}).reset_index()
-    worldData['Country'] = "world"
+    worldData['Country'] = "World"
     data = pd.concat([data, worldData])
     #data = data.rename(columns={'year': 'index'}).set_index('index')
     return data
@@ -30,19 +30,30 @@ data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text('')
 
+predURL = "https://ds20m008-autopublisher-project.azurewebsites.net/"
+
+
 year_to_filter = st.slider('year', min_value=1743, max_value=2013, step=1, value=2010)
 selected_country = "Italy"
-r = requests.get('http://localhost:8123/countries')
+r = requests.get(predURL+'countries')
 if r.status_code == 200:
     selected_country = st.selectbox("Which country to compare?", r.json()["countries"])
 
 
 fig = plt.figure()
 
-dsp = data[data["Country"] == "world"]
+dsp = data[data["Country"] == "World"]
 filtered = dsp[dsp["year"] < year_to_filter]
 sns.lineplot(data=dsp, x="year", y="AverageTemperature", alpha=0.3, palette=sns.color_palette(["#001c7f"]))
 sns.lineplot(data=filtered, x="year", y="AverageTemperature", alpha=1, palette=sns.color_palette(["#001c7e"]), label='World')
+preddata = filtered.sort_values(by="year",ascending=False)
+if preddata["year"].count() > 10:
+    body = list(preddata.head(10)["AverageTemperature"])
+    r = requests.post(predURL+"World", json=body)
+    if r.status_code == 200:
+        predresult = pd.DataFrame({"year": list(range(year_to_filter, year_to_filter+10)), "AverageTemperature": r.json()["temperatures_predicted"]})
+        sns.lineplot(data=predresult, x="year", y="AverageTemperature", alpha=1, palette=sns.color_palette(["#00FF00"]),
+                     label=selected_country + " prediction")
 
 
 dsp = data[data["Country"] == selected_country]
@@ -52,9 +63,9 @@ sns.lineplot(data=filtered, x="year", y="AverageTemperature", alpha=1, palette=s
 preddata = filtered.sort_values(by="year",ascending=False)
 if preddata["year"].count() > 10:
     body = list(preddata.head(10)["AverageTemperature"])
-    r = requests.post('http://localhost:8123/'+selected_country, json=body)
+    r = requests.post(predURL+selected_country, json=body)
     if r.status_code == 200:
-        predresult = pd.DataFrame({"year": list(range(year_to_filter+1, year_to_filter+11)), "AverageTemperature": r.json()["temperatures_predicted"]})
+        predresult = pd.DataFrame({"year": list(range(year_to_filter, year_to_filter+10)), "AverageTemperature": r.json()["temperatures_predicted"]})
         sns.lineplot(data=predresult, x="year", y="AverageTemperature", alpha=1, palette=sns.color_palette(["#00FF00"]),
                      label=selected_country + " prediction")
 
@@ -65,7 +76,7 @@ st.pyplot(fig)
 
 geoJSON_df = gpd.read_file("countries.geo.json")
 #countries = pd.read_csv("countries.geo.json")
-mapdata = data[(data["year"] == year_to_filter) & (data["Country"] != "world")][["Country", "AverageTemperature"]]
+mapdata = data[(data["year"] == year_to_filter) & (data["Country"] != "World")][["Country", "AverageTemperature"]]
 mapdata = mapdata.merge(geoJSON_df, left_on='Country', right_on='admin', how='left')[["Country","AverageTemperature","geometry"]]
 mapdata = mapdata.dropna(subset=["geometry"])
 geo = gpd.GeoSeries(mapdata.set_index('Country')['geometry']).to_json()
